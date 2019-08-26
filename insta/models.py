1029101496aa2f0040adbbc28101efafbdd8ac17
Abdalla Ashraf <abdallahgxr@gmail.com>
2019-08-26 20:12:45 +0800

    Done with the post_list view, template

--- insta/models.py
+++ insta/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    imgae  = models.ImageField(blank=True, null=True)
+    image  = models.ImageField(default='default.png', blank=True, null=True)
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
 
